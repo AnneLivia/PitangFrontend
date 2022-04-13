a45694dbab2e2cc5a3e144288383f375992a5258
Anne Livia <annelivia16@gmail.com>
2022-04-12 22:33:58 -0300

    toast container and context added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import AppointmentContextProvider from './contexts/AppointmentsContext';
 import Router from './Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      <div className='App'>
-        <Router />
-      </div>
+      <ToastContainer
+        position='top-right'
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        draggable
+      />
+      <AppointmentContextProvider>
+        <div className='App'>
+          <Router />
+        </div>
+      </AppointmentContextProvider>
     </>
   );
 }
