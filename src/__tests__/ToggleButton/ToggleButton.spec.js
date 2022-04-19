cf6fddf13f3533caea92e065f71affbb4d02b9dc
Anne Livia <annelivia16@gmail.com>
2022-04-19 13:35:18 -0300

    ToggleButton Component tests

---
+++ src/__tests__/ToggleButton/ToggleButton.spec.js
@@ -0,0 +1,26 @@
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CustomToggleButton from '../../components/ToggleButton';
+
+beforeEach(() => {
+    jest.spyOn(console, 'error')
+    console.error.mockImplementation(() => null);
+});
+  
+afterEach(() => {
+    console.error.mockRestore()
+});
+
+describe('ToggleButton Component', () => {
+    it('Should render the component without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<CustomToggleButton radioValue='2'/>, div);
+    });
+    it('Should render the toggle options', () => {
+       render(<CustomToggleButton radioValue='2'/>);
+       expect(screen.getByRole('radio', {name: 'Todos'})).toBeInTheDocument();
+       expect(screen.getByRole('radio', {name: 'Por Data Selecionada'})).toBeInTheDocument();
+    });
+});
